b6d9ab8e1f9b7f1c2e59108532d9d72a92c8d728
JMichal Rzyp <michalrzyp@gmail.com>
2019-09-24 12:14:52 +0200

    v1

---
+++ PingAsync/Form1.Designer.cs
@@ -0,0 +1,229 @@
+﻿namespace PingAsync
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(413, 268);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Add";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(210, 9);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Ping";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(4, 50);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(378, 186);
+            this.listBox1.TabIndex = 2;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(59, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(145, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(97, 250);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(54, 20);
+            this.numericUpDown1.TabIndex = 4;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(97, 276);
+            this.numericUpDown2.Maximum = new decimal(new int[] {
+            128,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(54, 20);
+            this.numericUpDown2.TabIndex = 5;
+            this.numericUpDown2.Value = new decimal(new int[] {
+            128,
+            0,
+            0,
+            0});
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(388, 50);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(100, 186);
+            this.listBox2.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(43, 252);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Repeat:";
+            this.label1.Click += new System.EventHandler(this.Label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(43, 278);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(30, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "TTL:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(395, 25);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Host list:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 15);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(48, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Address:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(388, 242);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 11;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(46, 302);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(286, 23);
+            this.button3.TabIndex = 12;
+            this.button3.Text = "Clear";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(500, 330);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Ping";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ PingAsync/Form1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.NetworkInformation;
+
+namespace PingAsync
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            if(textBox2.Text!=String.Empty)
+            {
+                if(textBox2.Text.Trim().Length>0)
+                {
+                    listBox2.Items.Add(textBox2.Text);
+                    textBox2.Clear();
+                }
+            }
+        }
+        private string SendPing(string address, int timeout, byte[] buffer, PingOptions option)
+        {
+            Ping ping = new Ping();
+            try
+            {
+                PingReply answer = ping.Send(address, timeout, buffer, option);
+                if (answer.Status == IPStatus.Success)
+                {
+                    return ("Answer with: " + address + " bajt=" + answer.Buffer.Length
+                       + " time=" + answer.RoundtripTime + "ms TTL=" + answer.Options.Ttl);
+                }
+                else
+                {
+                    return "Error: " + address + " " + answer.Status.ToString();
+                }
+            }
+            catch (Exception ex)
+            {
+                return "Error: " + address + " " + ex.Message;
+            }
+        }
+        private void SendPingAsync(string address, int timeout, byte[] buffer, PingOptions option)
+        {
+            Ping ping = new Ping();
+            ping.PingCompleted += new PingCompletedEventHandler(EndPing);
+            try
+            {
+                ping.SendAsync(address, timeout, buffer, option, null);
+            }
+            catch (Exception ex)
+            {
+                listBox1.Items.Add("Error: " + address + " " + ex.Message);
+            }
+        }
+        public void EndPing(object sender, PingCompletedEventArgs e)
+        {
+            if (e.Cancelled || e.Error != null)
+            {
+                listBox1.Items.Add("Operation aborted or wrong address");
+                ((IDisposable)(Ping)sender).Dispose();
+                return;
+            }
+            PingReply answer = e.Reply;
+            if (answer.Status == IPStatus.Success)
+            {
+                listBox1.Items.Add("Answer with: " + answer.Address.ToString() + " bajt=" + answer.Buffer.Length
+                   + " time=" + answer.RoundtripTime + "ms TTL=" + answer.Options.Ttl);
+            }
+            else
+            {
+                listBox1.Items.Add("Error: " + answer.Address.ToString() + " " + answer.Status.ToString());
+            }
+            ((IDisposable)(Ping)sender).Dispose();
+        }
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "" || listBox2.Items.Count > 0)
+            {
+                PingOptions option = new PingOptions();
+                option.Ttl = (int)numericUpDown2.Value;
+                option.DontFragment = true;
+                string data = "aaaaaaaaaaaaaaaaaa";
+                byte[] buffer = Encoding.ASCII.GetBytes(data);
+                int timeout = 120;
+                if (textBox1.Text != "")
+                {
+                    for(int i=0;i<(int)numericUpDown1.Value;i++)
+                    {
+                        //listBox1.Items.Add(this.SendPing(textBox1.Text, timeout, buffer, option));
+                        this.SendPingAsync(textBox1.Text, timeout, buffer, option);
+                        listBox1.Items.Add("-------------------");
+                    }
+                }
+                if (listBox2.Items.Count > 0)
+                {
+                    foreach(string host in listBox2.Items)
+                    {
+                        for (int i = 0; i < (int)numericUpDown1.Value; i++)
+                        {
+                           // listBox1.Items.Add(this.SendPing(host, timeout, buffer, option));
+                            this.SendPingAsync(host, timeout, buffer, option);
+                            listBox1.Items.Add("-------------------");
+                        }
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("No addresses provided", "Error");
+            }
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            listBox2.Items.Clear();
+            listBox1.Items.Clear();
+        }
+    }
+}
